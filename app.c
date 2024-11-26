29f4f9ec9b70373a5b4c43c1916aeb5b07173e2d
aryan30640 <aryan30640@gmail.com>
2024-11-26 21:02:16 +0530

    Updated

--- app.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("Hello, World!\n");
-    printf("This is my first code in C language.");
-    return 0;
-}
---
+++ basic.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello, World!\n");
+    printf("This is my first code in C language.\n");
+    printf("1\t2\t3\t\n4\t5\t6\t\n7\t8\t9\n");
+    printf("\"We should drink beer\" - Thomas Alva Edison(Maybe)");
+    return 0;
+}
