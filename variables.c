ba7f4554dc46e0fc1bf0146e1718bca5544f0642
aryan30640 <aryan30640@gmail.com>
2024-11-26 23:27:58 +0530

    Updated

--- variables.c
+++ variables.c
@@ -6,14 +6,14 @@ int main(){
     x = 6; //initialization
     int y = 12; //declaration + initialization
     int age = 24;
-    float gpa = 2.05; //floating point number(decimals)
-    char grade = 'C'; //single character
+    float profit = 2.05; //floating point number(decimals)
+    char grade = 'C'; //single character(letter)
     char name[] = "Satyam"; //array of characters
     //--
     printf("I am %d years old\n", age);
     printf("Hello %s\n", name);
     printf("Your average garde is %c\n", grade);
-    printf("Your GPA is %f\n", gpa);
+    printf("Your profit is %f\n", profit);
 
     return 0;
 }
