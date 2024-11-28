1620291542729273bae7a16692ec5f4eedbc6c02
aryan30640 <aryan30640@gmail.com>
2024-11-29 00:14:36 +0530

    Updated

--- variables.c
+++ variables.c
@@ -17,10 +17,15 @@ int main(){
     //--
     bool a = false; //true --> 1, false --> 0;
     signed char b = 100; //-128 to +127, signed char === char
-    unsigned char c = 250; //0 to +255
+    unsigned char c = 20; //0 to +255
+    short int d = 32000; //-32,768 to +32767
+    unsigned short int e = 65000; //0 to +65,535
+
     printf("The value of a is %d\n", a);
-    printf("The value of b is %c\n", b); // character --> integer
-    printf("The value of c is %d\n", c); //unsigned character --> integer
+    printf("The value of b is %c\n", b); // character --> integer, %c means character, %d can also be used here
+    printf("The value of c is %d\n", c); //unsigned character --> integer, %d is suggested here but %c can be used too
+    printf("the value of d is %d\n", d); //%d is used for integers
+    printf("the value of e is %d\n", e); //%d is used for integers
     return 0;
 }
 
