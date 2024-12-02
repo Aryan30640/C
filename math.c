9c1650dc0b35731dd121687629de199b32cef60e
aryan30640 <aryan30640@gmail.com>
2024-12-02 20:17:33 +0530

    Updated

--- math.c
+++ math.c
@@ -8,8 +8,6 @@ int main(){
     int d = ceil(3.14); //round off to higher value, ceil --> ceiling
     int e = floor(3.14); //round off to lower value,
     float f = fabs(-100); //tells the absolute value
-    float g = log(10);
-    float h = sin(90);
     //--
     printf("The square root of 729 is %.2f\n", a);
     printf("The valuse of 2 to the power 10 is %.2f\n", b);
@@ -17,7 +15,5 @@ int main(){
     printf("The we round off the value of PI for higher value, it would be %d\n", d);
     printf("The we round off the value of PI for lower value, it would be %d\n", e);
     printf("The absolute value of -100 is %.2f\n", f);
-    printf("The value of log 10 is %.2f\n", g);
-    printf("The value of sin 90 is %.2f\n", h);
     return 0;
 }
