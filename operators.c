c68797d095ecfc140c2834e835ccca7f32b05bf7
aryan30640 <aryan30640@gmail.com>
2024-12-02 17:32:51 +0530

    Updated

--- operators.c
+++ operators.c
@@ -4,6 +4,11 @@ int main(){
     int x = 5;
     int y = 2;
     float z = x/(float)y;
-    printf("%.2f", z);
+    printf("%.2f\n", z);
+    //--
+    int a = 5;
+    int b = 2;
+    int c = a % b;
+    printf("%d\n", c);
     return 0;
 }
